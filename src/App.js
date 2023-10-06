193fa7b7b7a6046ba819be0e8caacb4a816367e8
panhwar11090 <11090@kiet.edu.pk>
2023-10-06 22:32:57 +0500

    started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import Navbar from './Components/Layouts/Navbar';
 import './App.css';
 
 class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1>Hello world</h1>
+        <Navbar title = 'Github Finder' />
       </div>
     )
   }
